aab9e8f25d13d4810706e23710d76e6f5035e374
Tengda Han <htd95@126.com>
2020-10-03 22:58:26 +0100

    update InfoNCE and UberNCE

---
+++ model/classifier.py
@@ -0,0 +1,69 @@
+import sys
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+sys.path.append('../')
+from backbone.select_backbone import select_backbone
+
+
+class LinearClassifier(nn.Module):
+    def __init__(self, num_class=101, 
+                 network='resnet50', 
+                 dropout=0.5, 
+                 use_dropout=True, 
+                 use_l2_norm=False,
+                 use_final_bn=False):
+        super(LinearClassifier, self).__init__()
+        self.network = network
+        self.num_class = num_class
+        self.dropout = dropout
+        self.use_dropout = use_dropout
+        self.use_l2_norm = use_l2_norm
+        self.use_final_bn = use_final_bn
+        
+        message = 'Classifier to %d classes with %s backbone;' % (num_class, network)
+        if use_dropout: message += ' + dropout %f' % dropout
+        if use_l2_norm: message += ' + L2Norm'
+        if use_final_bn: message += ' + final BN'
+        print(message)
+
+        self.backbone, self.param = select_backbone(network)
+        
+        if use_final_bn:
+            self.final_bn = nn.BatchNorm1d(self.param['feature_size'])
+            self.final_bn.weight.data.fill_(1)
+            self.final_bn.bias.data.zero_()
+        
+        if use_dropout:
+            self.final_fc = nn.Sequential(
+                nn.Dropout(dropout),
+                nn.Linear(self.param['feature_size'], self.num_class))
+        else:
+            self.final_fc = nn.Sequential(
+                nn.Linear(self.param['feature_size'], self.num_class))
+        self._initialize_weights(self.final_fc)
+        
+    def forward(self, block):
+        (B, C, T, H, W) = block.shape
+        feat3d = self.backbone(block)
+        feat3d = F.adaptive_avg_pool3d(feat3d, (1,1,1)) # [B,C,1,1,1]
+        feat3d = feat3d.view(B, self.param['feature_size']) # [B,C]
+
+        if self.use_l2_norm:
+            feat3d = F.normalize(feat3d, p=2, dim=1)
+        
+        if self.use_final_bn:
+            logit = self.final_fc(self.final_bn(feat3d))
+        else:
+            logit = self.final_fc(feat3d)
+
+        return logit, feat3d
+
+    def _initialize_weights(self, module):
+        for name, param in module.named_parameters():
+            if 'bias' in name:
+                nn.init.constant_(param, 0.0)
+            elif 'weight' in name:
+                nn.init.normal_(param, 0.01)
+
---
+++ model/pretrain.py
@@ -0,0 +1,258 @@
+# MoCo-related code is modified from https://github.com/facebookresearch/moco
+import sys
+import math
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+sys.path.append('../')
+from backbone.select_backbone import select_backbone
+
+
+# utils
+@torch.no_grad()
+def concat_all_gather(tensor):
+    """
+    Performs all_gather operation on the provided tensors.
+    *** Warning ***: torch.distributed.all_gather has no gradient.
+    """
+    tensors_gather = [torch.ones_like(tensor)
+        for _ in range(torch.distributed.get_world_size())]
+    torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
+
+    output = torch.cat(tensors_gather, dim=0)
+    return output
+
+
+class InfoNCE(nn.Module):
+    '''
+    Basically, it's a MoCo for video input: https://arxiv.org/abs/1911.05722
+    '''
+    def __init__(self, network='s3d', dim=128, K=2048, m=0.999, T=0.07):
+        '''
+        dim: feature dimension (default: 128)
+        K: queue size; number of negative keys (default: 2048)
+        m: moco momentum of updating key encoder (default: 0.999)
+        T: softmax temperature (default: 0.07)
+        '''
+        super(InfoNCE, self).__init__()
+
+        self.dim = dim 
+        self.K = K
+        self.m = m
+        self.T = T
+
+        # create the encoders (including non-linear projection head: 2 FC layers)
+        backbone, self.param = select_backbone(network)
+        feature_size = self.param['feature_size']
+        self.encoder_q = nn.Sequential(
+                            backbone, 
+                            nn.AdaptiveAvgPool3d((1,1,1)),
+                            nn.Conv3d(feature_size, feature_size, kernel_size=1, bias=True),
+                            nn.ReLU(),
+                            nn.Conv3d(feature_size, dim, kernel_size=1, bias=True))
+
+        backbone, _ = select_backbone(network)
+        self.encoder_k = nn.Sequential(
+                            backbone, 
+                            nn.AdaptiveAvgPool3d((1,1,1)),
+                            nn.Conv3d(feature_size, feature_size, kernel_size=1, bias=True),
+                            nn.ReLU(),
+                            nn.Conv3d(feature_size, dim, kernel_size=1, bias=True))
+
+        for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
+            param_k.data.copy_(param_q.data)  # initialize
+            param_k.requires_grad = False  # not update by gradient
+
+        # create the queue
+        self.register_buffer("queue", torch.randn(dim, K))
+        self.queue = nn.functional.normalize(self.queue, dim=0)
+        self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
+
+        # Notes: for handling sibling videos, e.g. for UCF101 dataset
+
+
+    @torch.no_grad()
+    def _momentum_update_key_encoder(self):
+        '''Momentum update of the key encoder'''
+        for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
+            param_k.data = param_k.data * self.m + param_q.data * (1. - self.m)
+
+    @torch.no_grad()
+    def _dequeue_and_enqueue(self, keys):
+        # gather keys before updating queue
+        keys = concat_all_gather(keys)
+
+        batch_size = keys.shape[0]
+
+        ptr = int(self.queue_ptr)
+        assert self.K % batch_size == 0  # for simplicity
+
+        # replace the keys at ptr (dequeue and enqueue)
+        self.queue[:, ptr:ptr + batch_size] = keys.T
+        ptr = (ptr + batch_size) % self.K  # move pointer
+
+        self.queue_ptr[0] = ptr
+
+    @torch.no_grad()
+    def _batch_shuffle_ddp(self, x):
+        '''
+        Batch shuffle, for making use of BatchNorm.
+        *** Only support DistributedDataParallel (DDP) model. ***
+        '''
+        # gather from all gpus
+        batch_size_this = x.shape[0]
+        x_gather = concat_all_gather(x)
+        batch_size_all = x_gather.shape[0]
+
+        num_gpus = batch_size_all // batch_size_this
+
+        # random shuffle index
+        idx_shuffle = torch.randperm(batch_size_all).cuda()
+
+        # broadcast to all gpus
+        torch.distributed.broadcast(idx_shuffle, src=0)
+
+        # index for restoring
+        idx_unshuffle = torch.argsort(idx_shuffle)
+
+        # shuffled index for this gpu
+        gpu_idx = torch.distributed.get_rank()
+        idx_this = idx_shuffle.view(num_gpus, -1)[gpu_idx]
+
+        return x_gather[idx_this], idx_unshuffle
+
+    @torch.no_grad()
+    def _batch_unshuffle_ddp(self, x, idx_unshuffle):
+        '''
+        Undo batch shuffle.
+        *** Only support DistributedDataParallel (DDP) model. ***
+        '''
+        # gather from all gpus
+        batch_size_this = x.shape[0]
+        x_gather = concat_all_gather(x)
+        batch_size_all = x_gather.shape[0]
+
+        num_gpus = batch_size_all // batch_size_this
+
+        # restored index for this gpu
+        gpu_idx = torch.distributed.get_rank()
+        idx_this = idx_unshuffle.view(num_gpus, -1)[gpu_idx]
+
+        return x_gather[idx_this]
+
+    def forward(self, block):
+        '''Output: logits, targets'''
+        (B, N, *_) = block.shape # [B,N,C,T,H,W]
+        assert N == 2
+        x1 = block[:,0,:].contiguous()
+        x2 = block[:,1,:].contiguous()
+
+        # compute query features
+        q = self.encoder_q(x1)  # queries: B,C,1,1,1
+        q = nn.functional.normalize(q, dim=1)
+        q = q.view(B, self.dim)
+
+        in_train_mode = q.requires_grad
+
+        # compute key features
+        with torch.no_grad():  # no gradient to keys
+            if in_train_mode: self._momentum_update_key_encoder()  # update the key encoder
+
+            # shuffle for making use of BN
+            x2, idx_unshuffle = self._batch_shuffle_ddp(x2)
+
+            k = self.encoder_k(x2)  # keys: B,C,1,1,1
+            k = nn.functional.normalize(k, dim=1)
+
+            # undo shuffle
+            k = self._batch_unshuffle_ddp(k, idx_unshuffle)
+
+        k = k.view(B, self.dim)
+
+        # compute logits
+        l_pos = torch.einsum('nc,nc->n', [q, k]).unsqueeze(-1)
+        l_neg = torch.einsum('nc,ck->nk', [q, self.queue.clone().detach()])
+
+        # logits: B,(1+K)
+        logits = torch.cat([l_pos, l_neg], dim=1)
+
+        # apply temperature
+        logits /= self.T
+
+        # labels: positive key indicators
+        labels = torch.zeros(logits.shape[0], dtype=torch.long).cuda()
+        
+        # dequeue and enqueue
+        if in_train_mode: self._dequeue_and_enqueue(k)
+
+        return logits, labels
+
+
+class UberNCE(InfoNCE):
+    '''
+    UberNCE is a supervised version of InfoNCE,
+    it uses labels to define positive and negative pair
+    Still, use MoCo to enlarge the negative pool
+    '''
+    def __init__(self, network='s3d', dim=128, K=2048, m=0.999, T=0.07):
+        """
+        dim: feature dimension (default: 128)
+        K: queue size; number of negative keys (default: 2048)
+        m: moco momentum of updating key encoder (default: 0.999)
+        T: softmax temperature (default: 0.07)
+        """
+        super(UberNCE, self).__init__(network, dim, K, m, T)
+        # extra queue to store label
+        self.register_buffer("queue_label", torch.ones(K, dtype=torch.long) * -1)
+
+
+    def forward(self, block, k_label):
+        '''Output: logits, binary mask for positive pairs
+        '''
+        (B, N, *_) = block.shape # [B,N,C,T,H,W]
+        assert N == 2
+        x1 = block[:,0,:].contiguous()
+        x2 = block[:,1,:].contiguous()
+
+        # compute query features
+        q = self.encoder_q(x1)  # queries: B,C,1,1,1
+        q = nn.functional.normalize(q, dim=1)
+        q = q.view(B, self.dim)
+
+        in_train_mode = q.requires_grad
+
+        # compute key features
+        with torch.no_grad():  # no gradient to keys
+            if in_train_mode: self._momentum_update_key_encoder()  # update the key encoder
+
+            # shuffle for making use of BN
+            x2, idx_unshuffle = self._batch_shuffle_ddp(x2)
+
+            k = self.encoder_k(x2)  # keys: B,C,1,1,1
+            k = nn.functional.normalize(k, dim=1)
+
+            # undo shuffle
+            k = self._batch_unshuffle_ddp(k, idx_unshuffle)
+
+        k = k.view(B, self.dim)
+
+        # compute logits
+        l_pos = torch.einsum('nc,nc->n', [q, k]).unsqueeze(-1)
+        l_neg = torch.einsum('nc,ck->nk', [q, self.queue.clone().detach()])
+
+        # logits: B,(1+K)
+        logits = torch.cat([l_pos, l_neg], dim=1)
+
+        # apply temperature
+        logits /= self.T
+
+        # mask: binary mask for positive keys
+        mask = k_label.unsqueeze(1) == self.queue_label.unsqueeze(0) # B,K
+        mask = torch.cat([torch.ones((mask.shape[0],1), dtype=torch.long, device=mask.device).bool(),
+                          mask], dim=1) # B,(1+K)
+                
+        # dequeue and enqueue
+        if in_train_mode: self._dequeue_and_enqueue(k, k_label)
+
+        return logits, mask
